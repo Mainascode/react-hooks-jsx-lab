1f59e68cb08c41d17d2f0e4b03bd1cf541241d7c
Maina <smurkcarter855@gmail.com>
2024-10-31 13:47:49 +0300

    origin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import {name, city} from "../data/data.js";
 
+// function Home() {
+//   // update the JSX being returned!
+//   return <div id="home">
+//   <h1> {name} is a Web Developer from {city}</h1> 
+//  <style color="firebrick"></style>
+//   </div>
+ 
+
+// }
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home" className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 flex items-center justify-center">
+      <h1 
+        style={{ color: 'firebrick' }}
+        className="text-5xl font-bold text-center px-4 animate-fade-in"
+      >
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
+
+
 
 export default Home;
